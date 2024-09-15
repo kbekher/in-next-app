2b07e707f44eaa2790c2961a3c09891f76f91d6e
Kristina Bekher <krbekher@gmail.com>
2024-09-15 17:04:12 +0200

    Add preloader to Header

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -1,23 +1,29 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { useTranslation } from 'next-i18next'
-import Spline from '@splinetool/react-spline';
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+import { useTranslation } from "next-i18next";
+import Spline from "@splinetool/react-spline";
 
-import Header from '../Header/Header';
-import Navbar from '../Navbar/Navbar';
-import SayHello from '../SayHello/SayHello';
-import LangToggle from '../LangToggle/LangToggle';
+import Header from "../Header/Header";
+import Navbar from "../Navbar/Navbar";
+import SayHello from "../SayHello/SayHello";
+import LangToggle from "../LangToggle/LangToggle";
 
 const HeroSection = () => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
 
-  // Local state to track whether it's a desktop
+  const [isLoading, setIsLoading] = useState(true);
   const [isDesktop, setIsDesktop] = useState(false);
-
-  // Only run the media query on the client side
   const isDesktopQuery = useMediaQuery({ minWidth: 768 });
+
+  //TODO: make dinamic for Mobile & Desktop
+  const splineSrc = "https://prod.spline.design/liDjqaY-i3BfRC2o/scene.splinecode";
+
+  // Handle scene load event
+  const handleSceneLoaded = () => {
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     // Set the state after the component mounts
@@ -26,9 +32,18 @@ const HeroSection = () => {
 
   return (
     <div className='relative h-[100vh] w-full flex flex-col pt-[60px] px-5 pb-[36px] md:pt-10 md:px-20 md:pb-[70px]'>
+      {isLoading && (
+        <div className='absolute inset-0 flex justify-center items-center'>
+            <div class='p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-pink-400 via-purple-400 to-indigo-600 md:w-78 md:h-78 h-48 w-48 aspect-square rounded-full'>
+          {/* <div class='p-3 animate-spin drop-shadow-2xl bg-[linear-gradient(90deg,#F43640_0%,#A633AC_25%,#B207BA_50%,#933CD4_74%,#4481C1_100%,#1BA2B1_100%,#0FB0B4_100%)] md:w-78 md:h-78 h-48 w-48 aspect-square rounded-full'> */}
+            <div class='rounded-full h-full w-full bg-slate-100 bg-[#252525] background-blur-md'></div>
+          </div>
+        </div>
+      )}
+
       {/* Background Spline Object */}
       <div className='absolute inset-0 z-[-1]'>
-        <Spline scene='https://prod.spline.design/liDjqaY-i3BfRC2o/scene.splinecode' />
+        <Spline scene={splineSrc} onLoad={handleSceneLoaded} />
       </div>
 
       {/* Header */}
@@ -37,11 +52,10 @@ const HeroSection = () => {
       {/* Hero Content */}
       <div className='flex flex-col md:flex-row md:items-end justify-between flex-1'>
         <h1 className='md:max-w-[490px] text-4xl md:text-5xl font-bold select-none'>
-        {t('h1')}
+          {t("h1")}
         </h1>
 
         <div className='flex flex-wrap gap-3 md:block left-1/2 -translate-x-1/2 w-full md:left-auto md:-translate-x-0 w-full md:max-w-max fixed bottom-9 md:static box-border'>
-
           {!isDesktop && (
             <>
               <div className='md:hidden basis-full'>
@@ -52,13 +66,12 @@ const HeroSection = () => {
                 <SayHello />
               </div>
             </>
-            )}
+          )}
 
           <div className='flex-grow'>
             <LangToggle />
           </div>
         </div>
-
       </div>
     </div>
   );
