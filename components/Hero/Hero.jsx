69341f868f0d05676fbf3b987b71a9aef66839c1
Kristina Bekher <krbekher@gmail.com>
2024-12-08 21:19:40 +0100

    Update for safari

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -53,8 +53,8 @@ const Hero = () => {
           muted 
           loop 
         >
-          <source src={`${DOMAIN}spline-bg.webm`} type="video/webm" />
           <source src={`${DOMAIN}spline-bg.mp4`} type="video/mp4" />
+          <source src={`${DOMAIN}spline-bg.webm`} type="video/webm" />
           Your browser does not support the video tag.
         </video>
       </div>
