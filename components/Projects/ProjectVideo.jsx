2fef621645a3f9f9a0d67402c937d799a2449dd6
Kristina Bekher <krbekher@gmail.com>
2025-01-20 17:18:09 +0100

    Update project video component, play video once

--- components/Projects/ProjectVideo.jsx
+++ components/Projects/ProjectVideo.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { motion } from 'framer-motion';
 
@@ -7,62 +7,44 @@ import { scaleIn } from '@/utils/motion';
 
 const ProjectVideo = ({ name, assets }) => {
   const videoRef = useRef(null);
+  const isDesktop = useMediaQuery({ minWidth: 768 });
 
-  const [isDesktop, setIsDesktop] = useState(false);
-  const isDesktopQuery = useMediaQuery({ minWidth: 768 });
-
-  useEffect(() => {
-    setIsDesktop(isDesktopQuery);
-  }, [isDesktopQuery]);
+  const [hasPlayed, setHasPlayed] = useState(false);
 
   // Ensure a consistent `videoSrc` during SSR
   const videoSrc = useMemo(() => {
     return isDesktop && name === "xtrafit" ? assets[1] : assets[0];
   }, [isDesktop, name, assets]);
 
-  const handleScroll = useCallback(() => {
+  const handleEnter = (entry) => {
+    // console.log(entry.isIntersecting);
+    // console.log(entry.target);
+
     const videoElement = videoRef.current;
     if (!videoElement) return;
 
-    const videoRect = videoElement.getBoundingClientRect();
-    const midpoint = videoRect.top + videoRect.height / 2;
+    if (videoElement.paused && !hasPlayed) {
+      videoElement.play();
 
-    const isMidpointVisible =
-      midpoint >= window.innerHeight / 2 - 60 &&
-      midpoint <= window.innerHeight / 2 + 60;
-
-    const isInViewport =
-      videoRect.top < window.innerHeight && videoRect.bottom > 0;
-
-    if (isInViewport || isMidpointVisible) {
-      if (videoElement.paused || videoElement.currentTime === 0) {
-        videoElement.currentTime = 0;
-        videoElement.play();
-      }
+      setHasPlayed(true);
     }
-  }, []);
+  }
 
   useEffect(() => {
     const videoElement = videoRef.current;
 
-    if (videoElement) {
-      videoElement.currentTime = 0;
-      videoElement.load();
-    }
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []); //,videoSrc
+    if (videoElement) videoElement.load();
+    
+  }, [videoSrc]);
 
   return (
     <motion.div
       {...scaleIn(1.5)}
+      onViewportEnter={(entry) => handleEnter(entry)}
       className="project-video"
     >
       <video
+        data-played={hasPlayed}
         ref={videoRef}
         src={`${DOMAIN}${videoSrc}`}
         className="w-full h-auto scale-150"
