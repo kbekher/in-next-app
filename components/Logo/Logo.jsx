0241814cc57660b74d1b2a6bb95d803ca17a9ba7
Kristina Bekher <krbekher@gmail.com>
2025-10-03 23:19:46 +0200

    update mobile fonts

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -23,10 +23,6 @@ const Logo = ({ isClickable = false }) => {
       ) : (
         <div className='flex justify-center md:block'>
           <Image src={logoImage} alt="Logo" width={40} height={40} unoptimized />
-          {/* <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
-            <path d="M9 4C12.1696 4 14.7391 6.5695 14.7391 9.73913V26.9565C11.5695 26.9565 9 24.387 9 21.2174V4Z" fill="#040404" />
-            <circle cx="18.1303" cy="24.0869" r="2.86957" fill="#040404" />
-          </svg> */}
         </div>
       )}
     </>
